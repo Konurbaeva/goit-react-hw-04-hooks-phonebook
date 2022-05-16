45505d24eb0d887feadd0f1fe9093e43f1cebe5b
Konurbaeva <konurbaevamadina@gmail.com>
2022-05-16 09:22:59 +0200

    removed console.logs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -57,23 +57,16 @@ export class App extends Component {
   };
 
   componentDidMount(){
-    console.log('App componentDidMount');
     const contacts = localStorage.getItem('contacts')
     const parsedContacts = JSON.parse(contacts)
-
-    console.log('contacts from componentDidMount: ' + contacts);
-
     if(parsedContacts){
       this.setState({contacts: parsedContacts});
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('App componentDidUpdate');
 
     if(this.state.contacts !== prevState.contacts) {
-      console.log('contacts got updated');
-
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
